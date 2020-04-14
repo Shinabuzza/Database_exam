21d43a1672db49a4593c67e4bec1dac061a83770
Shinabuzza <31733836+Shinabuzza@users.noreply.github.com>
2020-04-14 16:27:18 +0300

    1

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -29,14 +29,14 @@ namespace WindowsFormsApp1
         {
             SQLiteCommand CMD = DB.CreateCommand();
             CMD.CommandText = "insert Into Kino(Film_name,First_name,Second_name,Film_Date, Country, Price,Income,Profit) values( @Film_name, @First_name, @Second_name,@Film_Date,@Country,@Price,@Income,@profit)";
-            CMD.Parameters.Add("@Film_name", System.Data.DbType.String).Value = textBox1.Text.ToUpper();
-            CMD.Parameters.Add("@First_name", System.Data.DbType.String).Value = textBox2.Text.ToUpper();
-            CMD.Parameters.Add("@Second_name", System.Data.DbType.String).Value = textBox3.Text.ToUpper();
-            CMD.Parameters.Add("@Film_Date", System.Data.DbType.String).Value = textBox4.Text.ToUpper();
-            CMD.Parameters.Add("@Country", System.Data.DbType.String).Value = textBox5.Text.ToUpper();
-            CMD.Parameters.Add("@Price", System.Data.DbType.Int32).Value = textBox6.Text.ToUpper();
-            CMD.Parameters.Add("@Income", System.Data.DbType.Int32).Value = textBox7.Text.ToUpper();
-            CMD.Parameters.Add("@Profit", System.Data.DbType.Int32).Value = textBox8.Text.ToUpper();
+            CMD.Parameters.Add("@Film_name", System.Data.DbType.String).Value = textBox1.Text;
+            CMD.Parameters.Add("@First_name", System.Data.DbType.String).Value = textBox2.Text;
+            CMD.Parameters.Add("@Second_name", System.Data.DbType.String).Value = textBox3.Text;
+            CMD.Parameters.Add("@Film_Date", System.Data.DbType.String).Value = textBox4.Text;
+            CMD.Parameters.Add("@Country", System.Data.DbType.String).Value = textBox5.Text;
+            CMD.Parameters.Add("@Price", System.Data.DbType.Int32).Value = textBox6.Text;
+            CMD.Parameters.Add("@Income", System.Data.DbType.Int32).Value = textBox7.Text;
+            CMD.Parameters.Add("@Profit", System.Data.DbType.Int32).Value = textBox8.Text;
             CMD.ExecuteNonQuery();
     }
 
@@ -58,6 +58,7 @@ namespace WindowsFormsApp1
         private void button2_Click(object sender, EventArgs e)
         {
             {
+                listBox1.Items.Clear();
                 SQLiteCommand CMD = DB.CreateCommand();
                 CMD.CommandText = "select * From Kino";
                 SQLiteDataReader SQL = CMD.ExecuteReader();
@@ -99,49 +100,30 @@ namespace WindowsFormsApp1
 
         private void button4_Click(object sender, EventArgs e)
         {
-
+           
+      
             SQLiteCommand CMD = DB.CreateCommand();
-            CMD.CommandText = ("DELETE  from Kino where 'Film_name' LIKE" + "'" + this.comboBox1.Text + "%'");
+            CMD.CommandType = CommandType.Text;
+            CMD.CommandText = "DELETE  from Kino where Film_name LIKE" +"'"+ this.comboBox1.Text + "%'";
 
-            SQLiteDataReader SQL = CMD.ExecuteReader();
-            if (SQL.HasRows)
-            {
-                while (SQL.Read())
-                {
-                    listBox1.Text += "Фильм: " + SQL["Film_name"] + " Имя : " + SQL["First_name"] + " Фамилия : " + SQL["Second_name"] + " Дата : " + SQL["Film_date"] + " Страна : " + SQL["Country"] + " Цена : " + SQL["Price"] + " Поступило : " + SQL["Income"] + " Профит : " + SQL["Profit"] + "\t\r\n";
-                }
-
-            }
-            else listBox1.Text = "Нет таких";
-
+            CMD.ExecuteNonQuery();
+            
         }
 
         private void button5_Click(object sender, EventArgs e)
 
         {
-           
+            listBox1.Items.Clear();
             SQLiteCommand CMD = DB.CreateCommand();
-            CMD.CommandText = ("Select * from Kino where 'Film_name' LIKE" + "'" + this.comboBox1.Text + "%'");
-            CMD.Parameters.Add("@Film_name", System.Data.DbType.String).Value = textBox1.Text.ToUpper();
-            CMD.Parameters.Add("@First_name", System.Data.DbType.String).Value = textBox2.Text.ToUpper();
-            CMD.Parameters.Add("@Second_name", System.Data.DbType.String).Value = textBox3.Text.ToUpper();
-            CMD.Parameters.Add("@Film_Date", System.Data.DbType.String).Value = textBox4.Text.ToUpper();
-            CMD.Parameters.Add("@Country", System.Data.DbType.String).Value = textBox5.Text.ToUpper();
-            CMD.Parameters.Add("@Price", System.Data.DbType.Int32).Value = textBox6.Text.ToUpper();
-            CMD.Parameters.Add("@Income", System.Data.DbType.Int32).Value = textBox7.Text.ToUpper();
-            CMD.Parameters.Add("@Profit", System.Data.DbType.Int32).Value = textBox8.Text.ToUpper();
-
+            CMD.CommandText = "select * From Kino where film_name LIKE" +"'"+this.textBox9.Text+ "%'";
             SQLiteDataReader SQL = CMD.ExecuteReader();
             if (SQL.HasRows)
             {
-                while (SQL.Read() )
-                {
-                    listBox1.Text+="Фильм: " + SQL["Film_name"] + " Имя : " + SQL["First_name"] + " Фамилия : " + SQL["Second_name"] + " Дата : " + SQL["Film_date"] + " Страна : " + SQL["Country"] + " Цена : " + SQL["Price"] + " Поступило : " + SQL["Income"] + " Профит : " + SQL["Profit"] + "\t\r\n";
+                while (SQL.Read())
+                {                    
+                    listBox1.Items.Add("Фильм: " + SQL["Film_name"] + " Имя : " + SQL["First_name"] + " Фамилия : " + SQL["Second_name"] + " Дата : " + SQL["Film_date"] + " Страна : " + SQL["Country"] + " Цена : " + SQL["Price"] + " Поступило : " + SQL["Income"] + " Профит : " + SQL["Profit"] + "\t\r\n");
                 }
-                
             }
-            else listBox1.Text = "Нет таких";
-
 
 
         }
